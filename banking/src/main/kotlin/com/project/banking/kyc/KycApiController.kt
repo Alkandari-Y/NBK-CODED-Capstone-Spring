7122599721d806e402b5ecce22686494c4849cb6
spdys <noorii549@yahoo.com>
2025-06-17 18:44:38 +0300

    moved kyc package

--- banking/src/main/kotlin/com/project/banking/kyc/KycApiController.kt
+++ banking/src/main/kotlin/com/project/banking/kyc/KycApiController.kt
@@ -1,16 +1,22 @@
-package com.project.banking.kycs
+package com.project.banking.kyc
 
 import com.project.banking.mappers.toResponse
-import com.project.common.data.requests.kyc.KYCRequest
-import com.project.common.data.responses.kyc.KYCResponse
 import com.project.banking.services.KYCService
+import com.project.common.data.requests.kyc.KYCRequest
 import com.project.common.data.responses.authentication.UserInfoDto
+import com.project.common.data.responses.kyc.KYCResponse
 import com.project.common.exceptions.kyc.KycNotFoundException
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestAttribute
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/kyc")
@@ -62,5 +68,3 @@ class KycApiController(
         )
     }
 }
-
-
