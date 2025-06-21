1222e68bd9d29c4d5b1b2d39cf9a29cffa54dec7
spdys <noorii549@yahoo.com>
2025-06-21 17:19:21 +0300

    whoopsie

--- common/src/main/kotlin/com/project/common/data/responses/promotions/PromotionResponse.kt
+++ common/src/main/kotlin/com/project/common/data/responses/promotions/PromotionResponse.kt
@@ -4,13 +4,13 @@ import com.project.common.enums.RewardType
 import java.time.LocalDate
 
 data class PromotionResponse(
-    val id: Long?,
+    val id: Long,
     val name: String,
-    val businessPartnerId: Long?,
-    val type: RewardType?,
-    val startDate: LocalDate?,
-    val endDate: LocalDate?,
+    val businessPartnerId: Long,
+    val type: RewardType,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
     val description: String,
     val storeId: Long?,
-    val xp: Long?
+    val xp: Long
 )
