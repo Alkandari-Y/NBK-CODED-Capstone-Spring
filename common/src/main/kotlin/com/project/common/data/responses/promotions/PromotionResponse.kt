a89eab82b19c594a5b5b349b8f8c469e7844d348
spdys <noorii549@yahoo.com>
2025-06-21 15:42:36 +0300

    rewardtype enum is so important y'all

--- common/src/main/kotlin/com/project/common/data/responses/promotions/PromotionResponse.kt
+++ common/src/main/kotlin/com/project/common/data/responses/promotions/PromotionResponse.kt
@@ -1,12 +1,13 @@
 package com.project.common.data.responses.promotions
 
+import com.project.common.enums.RewardType
 import java.time.LocalDate
 
 data class PromotionResponse(
     val id: Long?,
     val name: String,
     val businessPartnerId: Long?,
-    val type: Long?,
+    val type: RewardType?,
     val startDate: LocalDate?,
     val endDate: LocalDate?,
     val description: String,
