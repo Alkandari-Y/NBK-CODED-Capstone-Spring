7f39aaea49bd5008db62cfa273836e0adb3fab5f
spdys <noorii549@yahoo.com>
2025-06-26 20:29:50 +0300

    one business partner, one account

--- banking/src/main/kotlin/com/project/banking/entities/BusinessPartnerEntity.kt
+++ banking/src/main/kotlin/com/project/banking/entities/BusinessPartnerEntity.kt
@@ -8,30 +8,31 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "business_partners")
-data class BusinessPartnerEntity (
+data class BusinessPartnerEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-     var id: Long? = null,
+    var id: Long? = null,
 
     @Column(name = "name", nullable = false)
     var name: String = "",
 
     @Column(name = "admin_user", nullable = false)
-     var adminUser: Long? = null,
+    var adminUser: Long? = null,
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id", nullable = false)
     var account: AccountEntity? = null,
 
     @Column(name = "logo_url", nullable = false)
-     var logoUrl: String? = null,
+    var logoUrl: String? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
-     var category: CategoryEntity? = null,
+    var category: CategoryEntity? = null,
 )
