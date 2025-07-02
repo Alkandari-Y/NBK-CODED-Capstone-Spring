a8b1ae5dd078a584421751e0a4fc17ef29671612
Sarah <sarahalenezi96@outlook.com>
2025-07-02 05:20:24 +0300

    - added promo seeds

--- recommendation/src/main/kotlin/com/project/recommendation/config/DataSeeder.kt
+++ recommendation/src/main/kotlin/com/project/recommendation/config/DataSeeder.kt
@@ -8,11 +8,16 @@ import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import java.math.BigDecimal
 import java.time.LocalTime
+import com.project.recommendation.repositories.PromotionRepository
+import com.project.recommendation.entities.PromotionEntity
+import com.project.common.enums.RewardType
+import java.time.LocalDate
 
 
 @Component
 class DataSeeder(
-    private val storeLocationRepository: StoreLocationRepository
+    private val storeLocationRepository: StoreLocationRepository,
+    private val promotionRepository: PromotionRepository
 ) {
     @EventListener(ApplicationReadyEvent::class)
     @Transactional
@@ -21,6 +26,7 @@ class DataSeeder(
         Thread.sleep(8000)
         println("Starting data seeding...")
         seedStoreLocations()
+        seedPromotions()
     }
 
     fun seedStoreLocations() {
@@ -31,7 +37,7 @@ class DataSeeder(
 
         println("Seeded default locations.")
         val jumeirahHotel = StoreLocationEntity(
-            partnerId = 1, // reflects partner id
+            partnerId = 1,
             longitude = BigDecimal("29.268885427648737"),
             latitude = BigDecimal("48.08888399119982"),
             googleMapUrl = "https://maps.app.goo.gl/MpCyymSZptJc4W9x5",
@@ -226,6 +232,162 @@ class DataSeeder(
         storeLocationRepository.saveAll(listOf(
             jumeirahHotel, alMosafer, caribouArraya, shakeShack, kidZania, voxCinemas, kuwaitAirways, xcite,
             hAndM, safatHome, sparkGym, pickAlHamra, ofk, grandCinemas, chipsStore, sultanCenter
+        ))
+    }
+    fun seedPromotions() {
+        if (promotionRepository.count() > 0L) {
+            println("Promotions already exist. Skipping seeding.")
+            return
+        }
+
+        println("Seeding default promotions.")
+
+        val jumeirahPromotion = PromotionEntity(
+            name = "Luxury Stay Package",
+            businessPartnerId = 1,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-07-01"),
+            endDate = LocalDate.parse("2025-07-10"),
+            description = "20% off Presidential Suite. Experience world-class hospitality.",
+            storeId = 1
+        )
+
+        val almosaferPromotion = PromotionEntity(
+            name = "Early Bird Flight Deals",
+            businessPartnerId = 2,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-07-02"),
+            endDate = LocalDate.parse("2025-07-08"),
+            description = "Book your summer vacation to Greece early and save up to 25%",
+            storeId = 2
+        )
+
+        val caribouPromotion = PromotionEntity(
+            name = "Icy Summer, Icy Coffee",
+            businessPartnerId = 3,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-07-01"),
+            endDate = LocalDate.parse("2025-07-07"),
+            description = "Buy a large iced beverage and get a free soft serve. Perfect to beat the heat!",
+            storeId = 3
+        )
+
+        val shakeShackPromotion = PromotionEntity(
+            name = "Burger & Shake Combo Deal",
+            businessPartnerId = 4,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-07-03"),
+            endDate = LocalDate.parse("2025-07-09"),
+            description = "Get 15% off when you order any burger with a shake!",
+            storeId = 4
+        )
+
+        val voxPromotion = PromotionEntity(
+            name = "Mission Impossible Movie Week",
+            businessPartnerId = 6,
+            type = RewardType.CASHBACK,
+            startDate = LocalDate.parse("2025-07-04"),
+            endDate = LocalDate.parse("2025-07-06"),
+            description = "Get 10% cashback on tickets to Mission Impossible: Part Two. Make the impossible, possible!",
+            storeId = 6
+        )
+
+        val kuwaitAirwaysPromotion = PromotionEntity(
+            name = "Fly Kuwait Special Offer",
+            businessPartnerId = 7,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-07-02"),
+            endDate = LocalDate.parse("2025-07-7"),
+            description = "Special 20% discount on flights this weekend. Explore the world with comfort!",
+            storeId = 7
+        )
+
+        val xcitePromotion = PromotionEntity(
+            name = "Tech Summer Sale",
+            businessPartnerId = 8,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-07-01"),
+            endDate = LocalDate.parse("2025-07-14"),
+            description = "Up to 40% off on selected electronics. Upgrade your tech!",
+            storeId = 8
+        )
+
+
+        val hAndMPromotion = PromotionEntity(
+            name = "Summer Fashion Collection",
+            businessPartnerId = 9,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-06-28"),
+            endDate = LocalDate.parse("2025-07-05"),
+            description = "30% off on selected summer collection. Stay stylish and comfortable this season.",
+            storeId = 9
+        )
+
+        val safatHomePromotion = PromotionEntity(
+            name = "Home Makeover Special",
+            businessPartnerId = 10,
+            type = RewardType.CASHBACK,
+            startDate = LocalDate.parse("2025-07-02"),
+            endDate = LocalDate.parse("2025-07-16"),
+            description = "Earn 10% cashback when spending over 100 KD on home decor and furniture purchases",
+            storeId = 10
+        )
+
+        val sparkGymPromotion = PromotionEntity(
+            name = "Summer Fitness Challenge",
+            businessPartnerId = 11,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-07-06"),
+            endDate = LocalDate.parse("2025-07-13"),
+            description = "Join our summer fitness program and get 20% off membership fees. Get summer-ready!",
+            storeId = 11
+        )
+
+
+        val pickPromotion = PromotionEntity(
+            name = "Healthy Meal Deals",
+            businessPartnerId = 12,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-07-04"),
+            endDate = LocalDate.parse("2025-07-08"),
+            description = "15% off on all healthy meal options. Eat well, Feel well",
+            storeId = 12
+        )
+
+        val grandCinemasPromotion = PromotionEntity(
+            name = "Family Movie Night",
+            businessPartnerId = 14,
+            type = RewardType.DISCOUNT,
+            startDate = LocalDate.parse("2025-07-07"),
+            endDate = LocalDate.parse("2025-07-10"),
+            description = "25% off family tickets for selected shows.",
+            storeId = 14
+        )
+
+        val chipsStorePromotion = PromotionEntity(
+            name = "Gaming Gear Sale",
+            businessPartnerId = 15,
+            type = RewardType.CASHBACK,
+            startDate = LocalDate.parse("2025-07-02"),
+            endDate = LocalDate.parse("2025-07-09"),
+            description = "Get 8% cashback on gaming accessories and tech gadgets. Level up your setup!",
+            storeId = 15
+        )
+
+        val sultanCenterPromotion = PromotionEntity(
+            name = "Bulk Shopping Rewards",
+            businessPartnerId = 16,
+            type = RewardType.CASHBACK,
+            startDate = LocalDate.parse("2025-07-03"),
+            endDate = LocalDate.parse("2025-07-17"),
+            description = "Get 6% cashback on organic section and veggies purchases. Great deals on smart choices!",
+            storeId = 16
+        )
+
+        promotionRepository.saveAll(listOf(
+            jumeirahPromotion, almosaferPromotion, caribouPromotion, shakeShackPromotion, voxPromotion,
+            kuwaitAirwaysPromotion, xcitePromotion, hAndMPromotion, safatHomePromotion, sparkGymPromotion, pickPromotion,
+            grandCinemasPromotion, chipsStorePromotion, sultanCenterPromotion
         ))
     }
 }
